5e2d526bff734b4cda026102574a40581c672779
OmerXAI <omer.xxx.ai@gmail.com>
2023-08-10 22:10:19 +0200

    branch deneme

--- src/Deneme/cikartma.java
+++ src/Deneme/cikartma.java
@@ -3,5 +3,8 @@ package Deneme;
 public class cikartma {
     public static void main(String[] args) {
         System.out.println(" hub oldu mu?");
+
+
+        System.out.println(" buda 2. branch");
     }
 }
